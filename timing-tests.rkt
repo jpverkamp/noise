66bc6c60a6131d5377d5930a12410c5331c8949c
JP Verkamp <verkampj@indiana.edu>
2013-04-11 22:09:08 -0400

    A little more speed up using Float instead of Real but exposing a Real interface. It's about 10x as fast as it used to be.

--- timing-tests.rkt
+++ timing-tests.rkt
@@ -1,7 +1,41 @@
 #lang racket
 
-(require "noisy-image-test.rkt")
+(require 
+ "noise.rkt"
+ "noisy-image-test.rkt")
 
+(printf "--- images ---\n")
+
+(printf " perlin: ")
 (time (begin (build-perlin-image 256 256) (void)))
+
+(printf "simplex: ")
 (time (begin (build-simplex-image 256 256) (void)))
+
+(printf " colors: ")
 (time (begin (build-colored-simplex-image 256 256) (void)))
+
+(printf "\n--- no images ---\n")
+
+(printf " perlin: ")
+(time 
+ (for ([x (in-range 256)])
+   (for ([y (in-range 256)])
+     (perlin (/ x 256.0) (/ y 256.0))))
+ (void))
+
+(printf "simplex: ")
+(time 
+ (for ([x (in-range 256)])
+   (for ([y (in-range 256)])
+     (simplex (/ x 256.0) (/ y 256.0))))
+ (void))
+
+(printf " colors: ")
+(time 
+ (for ([x (in-range 256)])
+   (for ([y (in-range 256)])
+     (simplex (/ x 256.0) (/ y 256.0) -1.0)
+     (simplex (/ x 256.0) (/ y 256.0)  0.0)
+     (simplex (/ x 256.0) (/ y 256.0)  1.0)))
+ (void))
